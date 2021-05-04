71742d4a34c562fad8fc03a255ef8008d476add6
LisaUhl02 <7061@htl.rennweg.at>
2021-05-04 19:10:49 +0200

    v3_inserts

--- championship.sql
+++ championship.sql
@@ -16,12 +16,28 @@ CREATE TABLE IF NOT EXISTS player (
 DROP TABLE IF EXISTS rounds;
 CREATE TABLE IF NOT EXISTS rounds (
   pk_roundId INT PRIMARY KEY NOT NULL,
-  symbol VARCHAR(50) NOT NULL,
   date DATE,
   time TIME,
-  fk_playerId INT,
+  fk_playerId_1 INT,
+  symbolOfPlayer1 VARCHAR(50) NOT NULL,
+  fk_playerId_2 INT,
+  symbolOfPlayer2 VARCHAR(50) NOT NULL,
 
-  CONSTRAINT FOREIGN KEY fk_round_player (fk_playerId) REFERENCES player(pk_playerId)
+  CONSTRAINT FOREIGN KEY fk_round_player1 (fk_playerId_1) REFERENCES player(pk_playerId),
+  CONSTRAINT FOREIGN KEY fk_round_player2 (fk_playerId_2) REFERENCES player(pk_playerId)
 
 
 );
+
+
+INSERT INTO player VALUES
+(1,'Amy', 'Whinehouse', 27, 1,5),
+(2,'John', 'Lennon', 45, 2,7);
+
+INSERT INTO rounds VALUES
+(1, '2018-05-15','12:30', 1, 'Sissors', 2, 'Paper'),
+(2, '2018-05-15','12:40', 1, 'Rock', 2, 'Paper'),
+(3, '2018-05-15','12:50', 1, 'Sissors', 2, 'Rock'),
+(4, '2018-05-15','13:00', 1, 'Sissors', 2, 'Sissors'),
+(5, '2018-05-15','13:10', 1, 'Sissors', 2, 'Paper');
+
